cdc68790f20415edccda8205db38a28b8ac25b82
hinst <hinstance@yandex.ru>
2023-10-29 23:11:33 +0100

    Inform the caller about weird command line flags

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { parse } from 'https://deno.land/std@0.204.0/flags/mod.ts';
 import { App } from './app.ts';
 
-const flags = parse(Deno.args, {
-    boolean: ['update', 'summary'],
-    string: ['champion', 'advice'],
-});
+function main() {
+    const flags = parse(Deno.args, {
+        boolean: ['update', 'summary'],
+        string: ['champion', 'advice'],
+    });
+    if (!flags.update && !flags.summary && !flags.champion && !flags.advice) {
+        console.log('No flags recognized');
+        return -1;
+    }
+    new App(flags.update, flags.summary, flags.champion, flags.advice).run();
+}
 
-new App(flags.update, flags.summary, flags.champion, flags.advice).run();
+main();
