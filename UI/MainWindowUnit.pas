2f0b4b2e1282a3dcf3976eee7ceb5c5b80d63ecc
hinst <hinstance@yandex.ru>
2023-11-29 17:51:15 +0100

    Adding shortcut buttons in MainWindow

--- UI/MainWindowUnit.pas
+++ UI/MainWindowUnit.pas
@@ -5,7 +5,7 @@ unit MainWindowUnit;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Menus,
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Menus, Buttons,
   RTTICtrls, ConfigurationFrameUnit, SummaryFrameUnit, AdviceFrameUnit;
 
 type
@@ -13,6 +13,9 @@ type
   { TMainWindow }
 
   TMainWindow = class(TForm)
+    SummaryButton: TBitBtn;
+    ConfigurationButton: TBitBtn;
+    AdviceButton: TBitBtn;
     MainMenu1: TMainMenu;
     FileMenuItem: TMenuItem;
     ConfigurationMenuItem: TMenuItem;
@@ -20,10 +23,13 @@ type
     ResetWindowSizeMenuItem: TMenuItem;
     WindowMenuItem: TMenuItem;
     SummaryMenuItem: TMenuItem;
+    procedure AdviceButtonClick(Sender: TObject);
     procedure AdviceMenuItemClick(Sender: TObject);
+    procedure ConfigurationButtonClick(Sender: TObject);
     procedure ConfigurationMenuItemClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure ResetWindowSizeMenuItemClick(Sender: TObject);
+    procedure SummaryButtonClick(Sender: TObject);
     procedure SummaryMenuItemClick(Sender: TObject);
   private
     ActiveFrame: TFrame;
@@ -53,16 +59,32 @@ begin
   ActivateFrame(TAdviceFrame.Create(self));
 end;
 
-procedure TMainWindow.FormCreate(Sender: TObject);
+procedure TMainWindow.AdviceButtonClick(Sender: TObject);
 begin
   ClearFrame;
-  ActivateFrame(TSummaryFrame.Create(self));
+  ActivateFrame(TAdviceFrame.Create(self));
+end;
+
+procedure TMainWindow.ConfigurationButtonClick(Sender: TObject);
+begin
+  ClearFrame;
+  ActivateFrame(TConfigurationFrame.Create(self));
+end;
+
+procedure TMainWindow.FormCreate(Sender: TObject);
+begin
 end;
 
 procedure TMainWindow.ResetWindowSizeMenuItemClick(Sender: TObject);
 begin
   Width := 1200;
   Height := 700;
+end;
+
+procedure TMainWindow.SummaryButtonClick(Sender: TObject);
+begin
+  ClearFrame;
+  ActivateFrame(TSummaryFrame.Create(self));
 end;
 
 procedure TMainWindow.SummaryMenuItemClick(Sender: TObject);
