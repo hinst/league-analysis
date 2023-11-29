5aa458e13b44202795a4dcf0504e005ecddfc548
hinst <hinstance@yandex.ru>
2023-11-29 17:50:06 +0100

    Adding function CheckArrayEmpty

--- UI/StringUnit.pas
+++ UI/StringUnit.pas
@@ -8,6 +8,7 @@ uses
   Classes, SysUtils;
 
 function CheckArraysEqual(const a1: TStringArray; const a2: TStringArray): Boolean;
+function CheckArrayEmpty(const a: TStringArray): Boolean;
 
 implementation
 
@@ -21,6 +22,16 @@ begin
     if a1[i] <> a2[i] then
       Exit(false);
   result := true;
+end;
+
+function CheckArrayEmpty(const a: TStringArray): Boolean;
+var
+  s: string;
+begin
+  for s in a do
+    if s <> '' then
+      Exit(False);
+  Result := True;
 end;
 
 end.
